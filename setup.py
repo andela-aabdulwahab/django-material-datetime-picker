9f410a48b79ce0faf6db5a2adba5c35db70f3858
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2017-05-08 18:36:56 +0100

    Create a setup config

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name='django-material-datetime-picker',
+    version='0.0.1',
+    author='Abdulmalik Abdulwahab',
+    author_email='malik.adeyi@gmail.com',
+    description='Based on md-date-time-picker an implementaion of the the material picker component',
+    packages=['django_material_datetime_picker'],
+    include_package_data=True,
+    license='MIT',
+    install_requires=['django'],
+    package_data={'django_material_datetime_picker':
+                  ['static/md-date-time-picker/dist/css/*.css',
+                   'static/md-date-time-picker/dist/js/*.js']},
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
